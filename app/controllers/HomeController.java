0f6eb0d8a0f70929f8c31a763e0af6af800e3aaf
X00119916 <X00119916@computing.stu.it-tallaght.ie>
2016-11-21 12:27:06 +0000

    Ititial commit

---
+++ app/controllers/HomeController.java
@@ -0,0 +1,38 @@
+package controllers;
+
+import play.mvc.*;
+
+import views.html.*;
+
+/**
+ * This controller contains an action to handle HTTP requests
+ * to the application's home page.
+ */
+public class HomeController extends Controller {
+
+    /**
+     * An action that renders an HTML page with a welcome message.
+     * The configuration in the <code>routes</code> file means that
+     * this method will be called when the application receives a
+     * <code>GET</code> request with a path of <code>/</code>.
+     */
+    	public Result index() {
+        return ok(index.render());
+    }
+
+    	public Result news() {
+        return ok(news.render());
+    }
+
+	public Result downloads() {
+        return ok(downloads.render());
+    }		
+
+	public Result membership() {
+        return ok(membership.render());
+    }
+
+	public Result merchandise() {
+	return ok(merchandise.render());
+}
+}
